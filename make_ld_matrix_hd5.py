6b0af25cd1f249184171e996525f0612a8b52069
GenomicEPIOX <128259632+GenomicEPIOX@users.noreply.github.com>
2024-06-19 15:08:23 +0100

    added hd5 script

---
+++ make_ld_matrix_hd5.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import argparse
+import sys
+import warnings
+warnings.filterwarnings('ignore')
+
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Creates an LD matrix file in hd5 format to speed things up a little ' ,formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-i', '--input', help='The name of the vcor ld file that plink2 generates',required='True')
+    parser.add_argument('-o', '--output', help='output name default = Dhindsa_ld_matrix.hd5 ',default="Dhindsa_ld_matrix.hd5")
+    results = parser.parse_args(args)
+    return ( results.input, results.output) 
+
+def main(input_name, output): 
+    ld_matrix = pd.read_csv(input_file, sep="\t", low_memory=False)
+    ld_matrix.rename(columns={"#CHROM_A" : "CHR"} , inplace=True)
+    for i in range(1,23): 
+        temp = ld_matrix[ld_matrix["CHR"] == str(i) ]
+        #temp["key"] = temp.ID_A + "_" + temp.ID_B 
+        key_name = "chr" + str(i) 
+        temp.to_hdf(output, key=key_name, mode='a')
+    temp = ld_matrix[ld_matrix["CHR"] == "Y" ]
+    #temp["key"] = te mp.ID_A + "_" + temp.ID_B 
+    key_name = "chrY" 
+    temp.to_hdf(output, key=key_name, mode='a')
+    temp = ld_matrix[ld_matrix["CHR"] == "X" ]
+    #temp["key"] = temp.ID_A + "_" + temp.ID_B 
+    key_name = "chrX" 
+    temp.to_hdf(output, key=key_name, mode='a')
+
+
+
+if __name__ == '__main__':
+    input_file, output,  =check_arg(sys.argv[1:])
